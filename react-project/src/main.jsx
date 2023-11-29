a6442409737564e3087cf1ae48a06b557b2bd601
Matias <matielcruck1414@gmail.com>
2023-11-29 10:33:38 -0300

    main pronto y creando home con la lista de todos los jugadores

--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './Home.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/home",
+    element: <App/>,
+  },
+  {
+    path: "/jugador:id",
+    element: <App/>,
+  },
+  {
+    path: "/crear",
+    element: <App/>,
+  }
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
